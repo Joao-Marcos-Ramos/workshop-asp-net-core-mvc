f398cab8cae4d0e985c7389f313df6642a238e5a
João Marcos Ramos <jmramos625@gmail.com>
2019-01-06 15:43:59 -0800

    Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
@@ -9,14 +8,12 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-                                                     // Aqui já se faz a instanciação
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department()
         {
         }
 
-        // Em Construtores com arguntos não se inclui argumentos que são coleções.
         public Department(int id, string name)
         {
             Id = id;
