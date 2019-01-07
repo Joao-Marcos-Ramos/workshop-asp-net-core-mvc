c09573f342a6d8cd14aa734f4075a9c0adfdefe5
João Marcos Ramos <jmramos625@gmail.com>
2019-01-06 21:40:35 -0800

    Update seller and custom service exception

--- SalesWebMvc/Service/SellerService.cs
+++ SalesWebMvc/Service/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Service.Exceptions;
 
 namespace SalesWebMvc.Service
 {
@@ -37,6 +38,24 @@ namespace SalesWebMvc.Service
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            // O "Any" serve para dizer se há algum registro no banco de dados com a condição dada.
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
